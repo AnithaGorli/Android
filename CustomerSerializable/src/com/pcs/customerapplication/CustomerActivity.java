f2c0ae15465c0d9b426d561daac48107c5ef6791
Anitha <anitha.gorli@paradigmcreatives.com>
2014-09-10 12:06:24 +0530

    Changing Application to Serializable

---
+++ CustomerSerializable/src/com/pcs/customerapplication/CustomerActivity.java
@@ -0,0 +1,36 @@
+package com.pcs.customerapplication;
+
+import com.pcs.customerserializable.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class CustomerActivity extends Activity{
+	private Button Login_btn;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.customer);
+		Login_btn=(Button)findViewById(R.id.login_btn);
+
+
+		Login_btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				Intent intent=new Intent(CustomerActivity.this,LoginActivity.class);
+				startActivity(intent);
+
+
+			}
+		});
+
+	}
+}
